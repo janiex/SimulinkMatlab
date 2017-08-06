b5f4dae0913efdf41c1d926c4b1ee8db4ed4c711
Kris <kristian.dilov@gmail.com>
2017-08-06 20:04:34 +0300

    Improved ISO2631 validation to Validation_ISO2631.m. Constructed BS 6841 validation to Validation_BS_6841.m and added BS6841_simplified.xlsx input file.

--- Matlab/Validation_BS_6841.m
+++ Matlab/Validation_BS_6841.m
@@ -1,32 +1,29 @@
 clear all;
 clc
-print_multiple = 1;
-Wk_input = 2;
-Wd_input = 3;
-Wf_input = 4;
+print_multiple = 2;
+Wb_input = 2;
+Wc_input = 3;
+Wd_input = 4;
 
-Wc_input = 8;
-We_input = 9;
-Wj_input = 10;
+We_input = 5;
+Wf_input = 6;
+Wg_input = 7;
 
 f1_index = 1;
 
-Wk_Index = f1_index + 1;
-Wd_Index = Wk_Index + 3;
-Wf_Index = Wd_Index + 3;
-f2_index = Wf_Index + 3;
-Wc_Index = f2_index + 1;
-We_Index = Wc_Index + 3;
-Wj_Index = We_Index + 3;
-Coefficients = zeros(8,1);
+Wb_Index = f1_index + 1;
+Wc_Index = Wb_Index + 3;
+Wd_Index = Wc_Index + 3;
+We_Index = Wd_Index + 3;
+Wf_Index = We_Index + 3;
+Wg_Index = Wf_Index + 3;
+Coefficients = zeros(6,1);
 % This is the indexes of the report written, at later phase.
-Coefficients={Wk_Index, Wd_Index, Wf_Index,Wc_Index,We_Index,Wj_Index};
-InputFilters = {Wk_input, Wd_input, Wf_input, Wc_input, We_input, Wj_input};
+Coefficients={Wb_Index, Wc_Index, Wd_Index,We_Index,Wf_Index,Wg_Index};
+InputFilters = {Wb_input, Wc_input, Wd_input, We_input, Wf_input,Wg_input};
 Amp = 1; % Amplitude of the test signal
-xlsdata = xlsread('ISO2631_simplified.xlsx',1);
-%[num, txt, raw] = xlsread('ISO2631.xlsx',1);
-
-FinalFile = zeros(28,20);
+xlsdata = xlsread('BS6841_simplified',1);
+FinalFile = zeros(60,19);
 % number of iterations
 it = length(InputFilters);
 %*************************************************
@@ -34,96 +31,127 @@ it = length(InputFilters);
 %*************************************************
 
 FinalFile(:,f1_index)=xlsdata(:,1);
-FinalFile(:,f2_index)=xlsdata(:,7);
-Calculated = zeros(28,6);
-Tabled = zeros(28,6);
+Calculated = zeros(18,6);
+Tabled1 = zeros(18,6);
+Tabled2 = zeros(18,6);
+
 for count=1:it 
-    if count > 3        
-        %7 is the index of freq 2 in the input file
-        frequency = xlsdata(:,7); 
-        
-    else
-        %1 is the index of freq 1 in the input file
-        frequency = xlsdata(:,1);
-    end;
+    frequency = xlsdata(:,1);
     %remove trailing NaN in the array
     frequency(isnan(frequency(:,1)),:) = [];
     % Take the appropriate lentght of the block
     len = length(frequency);
+    %len = len/2;
     table_filter_index = cell2mat(InputFilters(count));
     W_Filter = xlsdata(:,table_filter_index); 
     position = cell2mat(Coefficients(1,count));
-   
-    for i=1:len
+    table_counter = 1;
+    for i=1:2:len
+    
         f = frequency(i);             % Signal frequency
         T = 1/f;                      % Window period on which the RMS will be applied0
         Fs = f*100;                   % Sampling frequency
         t = 0:1/Fs:200*T;             % Form the time window
         sig = Amp*sin(2*pi*f*t);      % Form the signal
-        temp = ISO2631(sig,count,Fs);     % Filter the signal with iso2631 filter
+        temp = bs6841(sig,count,Fs);     % Filter the signal with iso2631 filter
         
         %*************************************************
         % First write calculated by ISO2631 filter RMS
         %*************************************************
         FinalFile(i,position) = sqrt(mean(temp.^2)); %Implementation of the RMS function
-        Calculated(i,count) = FinalFile(i,position);
+        FinalFile(i+1,position) = FinalFile(i,position);
+        Calculated(table_counter,count) = FinalFile(i,position);
         % And now make the manual calculation on the given frequncy
         %*************************************************
         % now calculated by table in ISO2631
         %*************************************************
-        FinalFile(i,(position+1)) = ((Amp*W_Filter(i))/sqrt(2))/1000;
-        Tabled(i,count) = FinalFile(i,(position+1));
+        FinalFile(i,(position+1)) = ((Amp*W_Filter(i))/sqrt(2));        
+        Tabled1(table_counter,count) = FinalFile(i,(position+1));
+        
+        FinalFile(i+1,(position+1)) = ((Amp*W_Filter(i+1))/sqrt(2));        
+        Tabled2(table_counter,count) = FinalFile(i+1,(position+1));
+        table_counter = table_counter + 1;
+        
         %*************************************************
         if FinalFile(i,(position+1)) == 0
-            FinalFile(i,(position+2)) = 0;
+            FinalFile(i,(position+2)) = 0;    
         else
-            FinalFile(i,(position+2)) = (abs(FinalFile(i,(position+1))-FinalFile(i,(position))))/FinalFile(i,(position+1));    
-            FinalFile(i,(position+2)) = FinalFile(i,(position+2)) * 100;
+            FinalFile(i,(position+2)) = (abs(FinalFile(i,position)-FinalFile(i,(position+1)))/FinalFile(i,(position+1)))
+            FinalFile(i,(position+2)) = FinalFile(i,(position+2)) * 100; %100%
         end;
-        
+        if FinalFile(i+1,(position+1)) == 0
+            FinalFile(i+1,(position+2)) = 0;
+        else
+            FinalFile(i+1,(position+2)) = (abs(FinalFile(i,position)- FinalFile(i+1,(position+1)))/FinalFile(i+1,(position+1)));
+            FinalFile(i+1,(position+2)) = FinalFile(i+1,(position+2)) * 100;%100%
+        end;
     end;  
 end;
-Column_names_str = {'Freq [Hz]','Wk-Processed','Wk-Analitical','Wk-Error','Wd-Processed','Wd-Analitical','Wd-Error','Wf-Processed','Wf-Analitical','Wf-Error','Freq [Hz]','Wc-Processed','Wc-Analitical','Wc-Error','We-Processed','We-Analitical','We-Error','Wj-Processed','Wj-Analitical','Wj-Error'};
+Column_names_str = {'Freq [Hz]','Wb-Processed','Wb-Analitical','Wb-Error','Wc-Processed','Wc-Analitical','Wc-Error','Wd-Processed','Wd-Analitical','Wd-Error','We-Processed','We-Analitical','We-Error','Wf-Processed','Wf-Analitical','Wf-Error','Wg-Processed','Wg-Analitical','Wg-Error'};
     %output_matrix = [Column_names_str; FinalFile];
-    xlswrite('NewTestReport_ISO2631.xlsx',Column_names_str,'A1:T1'); 
-    xlswrite('NewTestReport_ISO2631.xlsx',FinalFile,'A2:T45');
-    Frequn = FinalFile(1:28,1);
-    Frequn1 = FinalFile(1:22,11);
-    Wk_Error = FinalFile(1:28,4);
-    Wd_Error = FinalFile(1:28,7);
-    Wf_Error = FinalFile(1:28,10);
-    Wc_Error = FinalFile(1:22,14);
-    We_Error = FinalFile(1:22,17);
-    Wj_Error = FinalFile(1:22,20);
-    
+    xlswrite('NewTestReport_BS_6841.xlsx',Column_names_str,'A1:T1'); 
+    xlswrite('NewTestReport_BS_6841.xlsx',FinalFile,'A2:T45');
+    Frequn = FinalFile(1:60,1);  
+    Wb_Error = FinalFile(1:60,4);
+    Wc_Error = FinalFile(1:60,7);
+    Wd_Error = FinalFile(1:60,10);
+    We_Error = FinalFile(1:60,13);
+    Wf_Error = FinalFile(1:60,16);
+    Wg_Error = FinalFile(1:60,19);
     %----------------------------------------
-    %         +++ Wk Error +++
+    %         +++ Wb Error +++
     %----------------------------------------
-    
+
     figure % opens new figure window
     if(print_multiple == 1)
         subplot(3,1,1)
     end;
-    plot(Frequn,Wk_Error)
-    title('Грешка за Wk(честота)')
+    plot(Frequn,Wb_Error)
+    title('Грешка за Wb(честота)')
     xlabel('Честота [Hz]') % x-axis label
     ylabel('Модул абсолютна грешка[%]') % y-axis label
     %----------------------------------------
     if(print_multiple == 1)
         subplot(3,1,2)    
         plot(Frequn,Tabled(:,1));
-        title('Wk Таблична стойност от стандарта')
+        title('Wb Таблична стойност от стандарта')
         xlabel('Честота [Hz]') % x-axis label
-        ylabel('Филтрирана с Wk ') % y-axis labe
+        ylabel('Филтрирана с Wb ') % y-axis labe
         subplot(3,1,3)
         plot(Frequn,Calculated(:,1));
-        title('Wk изчислена от филтера')
+        title('Wb изчислена от филтера')
         xlabel('Честота [Hz]') % x-axis label
-        ylabel('Филтрирана с Wk') % y-axis label
-    end; 
+        ylabel('Филтрирана с Wb') % y-axis label
+    end;
+    %----------------------------------------
+    %         +++ Wc Error +++
+    %----------------------------------------
+
+    figure % opens new figure window
+    if(print_multiple == 1)
+        subplot(3,1,1)
+    end;
+    plot(Frequn,Wc_Error)
+    title('Грешка за Wc(честота)')
+    xlabel('Честота [Hz]') % x-axis label
+    ylabel('Модул абсолютна грешка[%]') % y-axis label
+    %----------------------------------------
+    if(print_multiple == 1)
+        subplot(3,1,2)    
+        plot(Frequn,Tabled(:,1));
+        title('Wc Таблична стойност от стандарта')
+        xlabel('Честота [Hz]') % x-axis label
+        ylabel('Филтрирана с Wc ') % y-axis labe
+        subplot(3,1,3)
+        plot(Frequn,Calculated(:,1));
+        title('Wc изчислена от филтера')
+        xlabel('Честота [Hz]') % x-axis label
+        ylabel('Филтрирана с Wc') % y-axis label
+    end;
     %----------------------------------------
     %         +++ Wd Error +++
     %----------------------------------------
+
     figure % opens new figure window
     if(print_multiple == 1)
         subplot(3,1,1)
@@ -144,10 +172,36 @@ Column_names_str = {'Freq [Hz]','Wk-Processed','Wk-Analitical','Wk-Error','Wd-Pr
         title('Wd изчислена от филтера')
         xlabel('Честота [Hz]') % x-axis label
         ylabel('Филтрирана с Wd') % y-axis label
+    end; 
+    %----------------------------------------
+    %         +++ We Error +++
+    %----------------------------------------
+
+    figure % opens new figure window
+    if(print_multiple == 1)
+        subplot(3,1,1)
     end;
+    plot(Frequn,We_Error)
+    title('Грешка за We(честота)')
+    xlabel('Честота [Hz]') % x-axis label
+    ylabel('Модул абсолютна грешка[%]') % y-axis label
+    %----------------------------------------
+    if(print_multiple == 1)
+        subplot(3,1,2)    
+        plot(Frequn,Tabled(:,1));
+        title('We Таблична стойност от стандарта')
+        xlabel('Честота [Hz]') % x-axis label
+        ylabel('Филтрирана с We ') % y-axis labe
+        subplot(3,1,3)
+        plot(Frequn,Calculated(:,1));
+        title('We изчислена от филтера')
+        xlabel('Честота [Hz]') % x-axis label
+        ylabel('Филтрирана с We') % y-axis label
+    end; 
     %----------------------------------------
     %         +++ Wf Error +++
     %----------------------------------------
+
     figure % opens new figure window
     if(print_multiple == 1)
         subplot(3,1,1)
@@ -168,31 +222,7 @@ Column_names_str = {'Freq [Hz]','Wk-Processed','Wk-Analitical','Wk-Error','Wd-Pr
         title('Wf изчислена от филтера')
         xlabel('Честота [Hz]') % x-axis label
         ylabel('Филтрирана с Wf') % y-axis label
-    end;
-    %----------------------------------------
-    %         +++ Wc Error +++
-    %----------------------------------------
-    figure % opens new figure window
-    if(print_multiple == 1)
-        subplot(3,1,1)
-    end;
-    plot(Frequn1,Wc_Error)
-    title('Грешка за Wc(честота)')
-    xlabel('Честота [Hz]') % x-axis label
-    ylabel('Модул абсолютна грешка[%]') % y-axis label
-    %----------------------------------------
-    if(print_multiple == 1)
-        subplot(3,1,2)    
-        plot(Frequn,Tabled(:,1));
-        title('Wc Таблична стойност от стандарта')
-        xlabel('Честота [Hz]') % x-axis label
-        ylabel('Филтрирана с Wc ') % y-axis labe
-        subplot(3,1,3)
-        plot(Frequn,Calculated(:,1));
-        title('Wc изчислена от филтера')
-        xlabel('Честота [Hz]') % x-axis label
-        ylabel('Филтрирана с Wc') % y-axis label
-    end;
+    end; 
    %----------------------------------------
     %         +++ We Error +++
     %----------------------------------------
@@ -200,7 +230,7 @@ Column_names_str = {'Freq [Hz]','Wk-Processed','Wk-Analitical','Wk-Error','Wd-Pr
     if(print_multiple == 1)
         subplot(3,1,1)
     end;
-    plot(Frequn1,We_Error)
+    plot(Frequn,We_Error)
     title('Грешка за We(честота)')
     xlabel('Честота [Hz]') % x-axis label
     ylabel('Модул абсолютна грешка[%]') % y-axis label
@@ -218,26 +248,27 @@ Column_names_str = {'Freq [Hz]','Wk-Processed','Wk-Analitical','Wk-Error','Wd-Pr
         ylabel('Филтрирана с We') % y-axis label
     end;
     %----------------------------------------
-    %         +++ Wj Error +++
+    %         +++ Wg Error +++
     %----------------------------------------
+
     figure % opens new figure window
     if(print_multiple == 1)
         subplot(3,1,1)
     end;
-    plot(Frequn1,Wj_Error)
-    title('Грешка за Wj(честота)')
+    plot(Frequn,Wg_Error)
+    title('Грешка за Wg(честота)')
     xlabel('Честота [Hz]') % x-axis label
     ylabel('Модул абсолютна грешка[%]') % y-axis label
     %----------------------------------------
     if(print_multiple == 1)
         subplot(3,1,2)    
         plot(Frequn,Tabled(:,1));
-        title('Wj Таблична стойност от стандарта')
+        title('Wg Таблична стойност от стандарта')
         xlabel('Честота [Hz]') % x-axis label
-        ylabel('Филтрирана с Wj ') % y-axis labe
+        ylabel('Филтрирана с Wg ') % y-axis labe
         subplot(3,1,3)
         plot(Frequn,Calculated(:,1));
-        title('Wj изчислена от филтера')
+        title('Wg изчислена от филтера')
         xlabel('Честота [Hz]') % x-axis label
-        ylabel('Филтрирана с Wj') % y-axis label
-    end;
+        ylabel('Филтрирана с Wg') % y-axis label
+    end; 
--- Matlab/sperling.m
+++ Matlab/sperling.m
@@ -12,26 +12,49 @@ function [wz wr] = sperling(signal,fs)
 %--------------------------------------------------------------------------
 for ij=4:246
     f(ij)=ij*1000/8192;
-    % ride comfort weighting factor for horizontal direction
+    %**********************************************************************
+    % Ride comfort weightening factor for 
+    %                 horizontal direction 
+    %                 of the acceleration
+    %**********************************************************************
     Bw(ij)=0.737*((1.911*f(ij)^2+(0.25*f(ij)^2)^2)/ ...
     ((1-0.277*f(ij)^2)^2 + (1.563*f(ij)-0.0368*f(ij)^3)^2))^0.5;
-    % ride comfort weighting factor for vertical direction
+    %**********************************************************************
+    % Ride comfort weightening factor for 
+    %                 vertical direction 
+    %                 of the acceleration
+    %**********************************************************************
     Bs(ij)=Bw(ij)/1.25;
+    %**********************************************************************
     aa = f(ij);
     bb =(1 - 0.056*aa^2)^2;
     cc= ( (0.645*aa)^2 )* (3.55*aa^2);
     dd = (1- 0.252*aa^2)^2 ;
     ee = (1.547*aa - 0.00444*aa^3)^2 ;
     ff = 1 + 3.55*aa^2;
-    %ride quality weighting factor
-    B(ij) = 1.14*(( bb+ cc )/((dd +ee)*ff))^0.5; ;
+  
+    %**********************************************************************
+    % Vehicle ride quality
+    %**********************************************************************
+    B(ij) = 1.14*(( bb+ cc )/((dd +ee)*ff))^0.5;
 end
+hold on;
+plot(f(4:end),Bw(4:end),'r');
+plot(f(4:end),Bs(4:end),'g');
+plot(f(4:end),B(4:end),'b');
+set(gca,'fontsize',18)
+xlabel('Честота([hz]');
+ylabel('Коефициент на предаване');
+hleg1 = legend('Влияние на трептения върху комфорта в хоризонтална посока','Влияние на трептения върху комфорта в вертикална посока','Влияние на трептения върху планост на хода');
+set(gca,'fontsize',24)
+title('Индекс на Шперлинг - тегловни предавателни характеристики');
 % Freq = w/2*pi;
-% semilogx(Freq,20*log10(hn),'m');
+%semilogx(f,20*log10(Bs),'m');
+%semilogx(f,20*log10(Bs),'m');
 % plot(f(4:end),Bw(4:end));
 % xlabel('frequency(hz)');
-% ylabel('Bs-comfort Horizontal direction');
-% title('sperling index frequency weughting curve ');
+
+
 for a=1:3
     sum=0;sum2=0;
     [Pxx,w] = pwelch(signal(:,a),[],[],8192,fs);
